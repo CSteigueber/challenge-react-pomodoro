d4c766346aa7ea41a7d3aba3dea75a2613e2559c
CSteigueber <c.steigueber@gmail.com>
2020-01-29 14:46:58 +0100

    reset button and PLus/Minus buttons implemented

--- src/index.js
+++ src/index.js
@@ -12,15 +12,35 @@ class Time {
 class Clock extends React.Component{
     constructor (props) {
         super(props);
-        this.state= {seconds:1200,
+        this.initial=1200;
+        this.state= {seconds:this.initial,
                      running: false,
         };
-        this.handleClick = this.handleClick.bind(this);
+        this.handleStartResetClick = this.handleStartResetClick.bind(this);
+        this.handlesPlusClick = this.handlesPlusClick.bind(this);
+        this.handlesMinusClick = this.handlesMinusClick.bind(this);
     }
-    handleClick(){
+    handleStartResetClick(){
+        if (this.state.running){
+            this.setState({
+                seconds: this.initial
+            })
+        }
         this.setState({
             running: !this.state.running
         });
+    }
+    handlesPlusClick(){
+        if (this.state.running){
+            return;
+        }
+        this.setState({seconds: this.state.seconds +60});
+    }
+    handlesMinusClick(){
+        if (this.state.running){
+            return;
+        }
+        this.setState({seconds: this.state.seconds -60});
     }
     componentDidMount(){
         this.timerID = setInterval( ()=>this.tick(), 1000);
@@ -29,15 +49,20 @@ class Clock extends React.Component{
         clearInterval(this.timerID);
     }
     tick(){
-        if (this.state.running){
+        if (this.state.running && this.state.seconds>0){
             this.setState({seconds: this.state.seconds-1});
         }
     }
     render(){
-    return (<div><h1>{Math.floor(this.state.seconds/60 < 10 ? '0'+this.state.seconds/60 : this.state.seconds/60)}
-        :{this.state.seconds%60 < 10 ? '0'+this.state.seconds%60 : this.state.seconds%60}</h1>
-        <button onClick={this.handleClick}>{this.state.running ? 'Stop' : 'Start'}</button>
-        </div>
+    return (<div>
+                <h1>
+                    {Math.floor(this.state.seconds/60 < 10 ? '0'+this.state.seconds/60 : this.state.seconds/60)}
+                    :{this.state.seconds%60 < 10 ? '0'+this.state.seconds%60 : this.state.seconds%60}
+                </h1>
+                <button onClick={this.handleStartResetClick}>{this.state.running ? 'Reset' : 'Start'}</button>
+                <button onClick={this.handlesPlusClick}>+</button>                
+                <button onClick={this.handlesMinusClick}>-</button>
+            </div>
     );
  
     }
