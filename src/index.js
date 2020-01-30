305bb91947272cbb87910af3fb5ffa7a4a5a5a93
CSteigueber <c.steigueber@gmail.com>
2020-01-30 11:24:28 +0100

    slide refactoring

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import * as serviceWorker from './serviceWorker';
-// import App from './App';
-// import { ReactComponent } from '*.svg';
-
 class Clock extends React.Component{
     constructor (props) {
         super(props);
@@ -83,17 +79,4 @@ class Clock extends React.Component{
     }
 }
 
-class App extends React.Component {
-
-    render(){
-        return <Clock />;
-    }
-}
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+ReactDOM.render(<Clock />, document.getElementById('root'));
